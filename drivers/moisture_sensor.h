ee66aecf22233014d37cdff72ea1f60bd55a6b1d
EkoLunde <ekolail@gmail.com>
2022-11-16 14:27:32 -0800

    Chaotically getting started with sensor driver

--- drivers/moisture_sensor.h
+++ drivers/moisture_sensor.h
@@ -5,8 +5,23 @@
 
 
 
-void sensor_init(uint8_t gpio_num, gpio_direction_t dir){
-    gpio_config(gpio_num, dir);
 
+typedef struct {
+    uint8_t id;
+    uint16_t sensitivity;
+    uint16_t bias;
+    uint16_t adc_val;
+} moisture_sensor_t;
 
+void init(moisture_sensor_t *sensor, uint16_t id, uint16_t sens, uint16_t bias, uint16_t adc){
+    sensor->id = id;
+    sensor->sensitivity = sens;
+    sensor->bias = bias;
+    sensor->adc_val = adc;
 }
+
+uint
+
+
+
+
