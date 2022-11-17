bb988037e53b33cc09b98b62c8ad3fbbd4bde8ec
sriramsridhar23 <77755889+sriramsridhar23@users.noreply.github.com>
2022-11-16 18:43:56 -0800

    Add files via upload

---
+++ drivers/mrplant/drivers/nrf_drv_config.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "nrf_gpio.h"
+#include "nrf_saadc.h"
+
+#define BUTTON1     NRF_GPIO_PIN_MAP(0,13)
+#define BUTTON2     NRF_GPIO_PIN_MAP(0,14)
+#define BUTTON3     NRF_GPIO_PIN_MAP(0,15)
+#define BUTTON4     NRF_GPIO_PIN_MAP(0,16)
+
+#define LED1        NRF_GPIO_PIN_MAP(0,17)
+#define LED2        NRF_GPIO_PIN_MAP(0,18)
+#define LED3        NRF_GPIO_PIN_MAP(0,19)
+#define LED4        NRF_GPIO_PIN_MAP(0,20)
+
+#define UART_RXD    NRF_GPIO_PIN_MAP(0,8)
+#define UART__TXD   NRF_GPIO_PIN_MAP(0,7)
+
+#define PIN2        NRF_GPIO_PIN_MAP(0,2)
+#define PIN3        NRF_GPIO_PIN_MAP(0,3)
+#define PIN4        NRF_GPIO_PIN_MAP(0,4)
+#define PIN11       NRF_GPIO_PIN_MAP(0,11)
+#define PIN12       NRF_GPIO_PIN_MAP(0,12)
+#define PIN22       NRF_GPIO_PIN_MAP(0,22)
+#define PIN23       NRF_GPIO_PIN_MAP(0,23)
+#define PIN24       NRF_GPIO_PIN_MAP(0,24)
+#define PIN25       NRF_GPIO_PIN_MAP(0,25)
+#define PIN26       NRF_GPIO_PIN_MAP(0,26)
+#define PIN27       NRF_GPIO_PIN_MAP(0,27)
+#define PIN28       NRF_GPIO_PIN_MAP(0,28)
+#define PIN29       NRF_GPIO_PIN_MAP(0,29)
+#define PIN30       NRF_GPIO_PIN_MAP(0,30)
+#define PIN31       NRF_GPIO_PIN_MAP(0,31)
+
