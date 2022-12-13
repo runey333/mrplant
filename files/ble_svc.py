af2f5a14517548bda13bfc1542a6a8b922fc7f91
runey333 <arun.sundaresan1@gmail.com>
2022-12-13 10:57:12 -0800

    continue BLE

--- files/ble_svc.py
+++ files/ble_svc.py
@@ -1,11 +1,18 @@
 from flask import Flask
 import asyncio
-from bleak import BleakClient
-from bleak import BleakScanner, BleakClient
+from bleak import BleakScanner, BleakClient, BleakError
+from ble_utils import parse_ble_args, handle_sigint
+import sys
 
-app = Flask(__name__)
+args = parse_ble_args('Print advertisement data from a BLE device')
+addr = args.addr.lower()
+timeout = args.timeout
+handle_sigint()
 
 moisture_level = 500
+SERVICE_UUID = "790a1915-6284-4d1a-957d-23be708470bb"
+
+app = Flask(__name__)
 
 async def find():
     devices = await BleakScanner.discover()
@@ -16,12 +23,32 @@ async def find():
         #    print(f"Address: {d.address}")
         #    print("       Model Number: {0}".format("".join(map(chr, model_number))))
 
+
+async def set_moisture(address, level):
+    print(f"searching for device {address} ({timeout}s timeout)")
+    try:
+        async with BleakClient(address,timeout=timeout) as client:
+            print("connected")
+            loop = asyncio.get_event_loop()
+            print("Current event loop from the main thread" + str(loop))
+            try:
+                future = asyncio.run_coroutine_threadsafe(client.write_gatt_char(SERVICE_UUID, bytes(level), loop))
+            except:
+                print("An error occurred")
+            finally:
+                await client.disconnect()
+                print("The client has disconnected.")
+    except BleakError as e:
+        print("not found")
+
 @app.route("/set/<level>", methods=['POST'])
 def set_moisture(level):
+    global moisture_level
     moisture_level = level
+    set_moisture(addr, moisture_level)
     return {"result" : f"set to {moisture_level}"}
     
 @app.route("/get", methods=['GET'])
 def get_moisture():
-    asyncio.run(find())
+    #asyncio.run(find())
     return {"result" : str(moisture_level)}
--- files/frontend.py
+++ files/frontend.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import flask
 import requests
-import json
 
 def get_moisture():
     response = requests.get("http://127.0.0.1:5000/get")
