4c760f9562bb80e8f678b0dcf87667fa8271b037
sriramsridhar23 <77755889+sriramsridhar23@users.noreply.github.com>
2022-12-20 15:49:24 -0800

    Add files via upload

--- files/ble_svc.py
+++ files/ble_svc.py
@@ -6,10 +6,8 @@ import sys
 
 app = Flask(__name__)
 
-#args = parse_ble_args('Print advertisement data from a BLE device')
 addr = "c0:98:e5:49:00:03"
 timeout = 10.0
-#handle_sigint()
 
 moisture_level = 500
 SERVICE_UUID = "790a1915-6284-4d1a-957d-23be708470bb"
@@ -18,11 +16,6 @@ async def find():
     devices = await BleakScanner.discover()
     for d in devices:
         print(d)
-        #async with BleakClient(d.address) as client:
-        #    model_number = await client.read_gatt_char(MODEL_NBR_UUID, timeout=5.0)
-        #    print(f"Address: {d.address}")
-        #    print("       Model Number: {0}".format("".join(map(chr, model_number))))
-
 
 async def set_moisture_level(address, level):
     print(f"searching for device {address} ({timeout}s timeout)")
@@ -46,12 +39,10 @@ def set_moisture(level):
     if (int(level) >= 0 and int(level) <= 700):
         global moisture_level
         moisture_level = int(level)
-        #set_moisture(addr, moisture_level)
         return {"result" : f"set to {moisture_level}"}
     else:
         return {"result" : f"Invalid level"}
     
 @app.route("/get", methods=['GET'])
 def get_moisture():
-    #asyncio.run(find())
     return {"result" : str(moisture_level)}
--- files/test.py
+++ files/test.py
@@ -10,8 +10,4 @@ async def main(address):
     for d in devices:
         print(d)
 
-    # async with BleakClient(address) as client:
-    #     model_number = await client.read_gatt_char(MODEL_NBR_UUID)
-    #     print("Model Number: {0}".format("".join(map(chr, model_number))))
-
 asyncio.run(main(address))
